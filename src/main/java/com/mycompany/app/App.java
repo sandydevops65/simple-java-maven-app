ce57beb361ffdf95224bb7bc9ede243df5d35e0e
sandydevops65 <38243215+sandydevops65@users.noreply.github.com>
2020-01-10 15:33:36 +0530

    Hello world updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World123!";
 
     public App() {}
 
@@ -17,5 +17,5 @@ public class App
     private final String getMessage() {
         return message;
     }
-
+    
 }
